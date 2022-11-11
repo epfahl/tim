8caa0ee6be4efe1dd97e96e36a4d44f492bdb906
Eric Pfahl <eric.pfahl@gmail.com>
2022-11-10 21:24:32 -0500

    add a unit test for basic functionality of `time`

--- test/tim_test.exs
+++ test/tim_test.exs
@@ -1,4 +1,16 @@
 defmodule TimTest do
   use ExUnit.Case
   doctest Tim
+
+  test "sleep, test result and mean" do
+    map =
+      :timer.sleep(1)
+      |> case do
+        :ok -> 1
+      end
+      |> Tim.time(10)
+
+    assert map.result == 1
+    assert map.mean <= 3_000
+  end
 end
