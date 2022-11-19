accaebde308700aefa4a525ba9ed7aaf70da0abf
Eric Pfahl <eric.pfahl@gmail.com>
2022-11-19 15:00:45 -0500

    add module doc; update time doc

--- lib/tim.ex
+++ lib/tim.ex
@@ -1,11 +1,56 @@
 defmodule Tim do
   @moduledoc """
+  `Tim` provides the macro `time` that takes any valid Elixir expression and returns a map
+  containing several statistics for the expression's execution time, the result of the
+  evaluated expression, and the expression's string representation. To use
+  `time`, require or import `Tim` into your environment and pipe in the expression:
+
+  ```elixir
+  require Tim
+
+  1..10
+  |> Enum.map(& &1 * 2/((1 + 10) * 10))
+  |> Enum.sum()
+  |> Tim.time()
+  ```
+
+  This returns (actual timing numbers will vary)
+
+  ```elixir
+  %{
+    expr: "1..10 |> Enum.map(&(&1 * 2 / ((1 + 10) * 10))) |> Enum.sum()",
+    max: 46,
+    mean: 46.0,
+    median: 46,a
+    min: 46,
+    n: 1,
+    result: 1.0
+  }
+  ```
+  All times are in microseconds.
+
+  The `time` macro takes a second argument that is the number `n` of times the expression
+  is executed to gather timing statistics. When the above expression is piped into
+  `Time.time(100)`, the result will look something like
+
+  ```elixir
+  %{
+    expr: "1..10 |> Enum.map(&(&1 * 2 / ((1 + 10) * 10))) |> Enum.sum()",
+    max: 71,
+    mean: 13.83,
+    median: 12,
+    min: 10,
+    n: 100,
+    result: 1.0
+  }
+  ```
   """
 
   alias Tim.Stats
 
   @doc """
-  Return a collection of timing stats after executing the provided expression `n` times.
+  Takes an Elixir expression and returns a map of timing stats after executing
+  the provided expression `n` times.
   """
   defmacro time(expr, n \\ 1) when is_integer(n) do
     expr_string = Macro.to_string(expr)
